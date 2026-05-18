e9dd49a2bfb5eb5afd2099b7505ddda30c4c41cf
Festongithub <fkemboik@gmail.com>
2026-05-18 11:26:21 +0300

    Activation function

--- basic_ml/pi.py
+++ basic_ml/pi.py
@@ -3,8 +3,10 @@
 import numpy as np
 
 
-
-inputs = [1, 2, 3, 2.5]
+#inputs can be features from a heat-detection centers
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.8, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]] 
 
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
@@ -12,8 +14,22 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
-print(output)
+weights2 = [[0.1, -0.14, 0.5],
+           [0.5, 0.12, -0.33],
+           [0.44, 0.73, -0.13]]
+
+biases2 = [1, 2, -0.5]
+
+
+
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
+
+#u1 = np.dot(weights, np.array(inputs).T) + biases
+#print(u1)
 
 
 some_value = 0.5
