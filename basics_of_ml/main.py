de2bef8f4f00cf8e373119109a591149e8bf3c87
Festongithub <fkemboik@gmail.com>
2026-05-21 02:08:52 -0700

    Pre batch

--- basics_of_ml/main.py
+++ basics_of_ml/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from basics-of-ml!")
+import numpy as np
 
+l = [1, 2, 3]
+weights = [5, 3, 2]
+bias = 2 * 2
 
-if __name__ == "__main__":
-    main()
+output = l[0] * weights[0] + l[1] * weights[1] + l[2] * weights[2] + bias
+print(output)
+
+# Tuning weights and biases to accurately work better
+
+inputs = [1, 2, 3, 2.5]
+
+w = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]]
+
+b = 2,
+b1 = 3,
+b2 = 0.5
+
+total = [
+    
+]
