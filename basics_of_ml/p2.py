7c7d3021a06441ff33556a6996dfcb9a65a92ea1
Festongithub <fkemboik@gmail.com>
2026-05-26 03:31:58 -0700

    model building phase 1

--- basics_of_ml/p2.py
+++ basics_of_ml/p2.py
@@ -1,7 +1,7 @@
 import numpy as np
 np.random.seed(0)
 import nnfs
-from nnfs.datasets import spiral_data
+from nnfs.datasets import spiral_data, vertical_data
 
 nnfs.init()
 
@@ -73,3 +73,55 @@ if __name__ =='__main__':
     loss = loss_function.calculate(activation2.output, y)
 
     print("Loss: ", loss)
+
+
+
+    A, b = vertical_data(samples=100, classes=3)
+
+    denseA = Layer_Dense(2, 3)
+    activationA = Activation_ReLU()
+
+    denseB = Layer_Dense(3, 3)
+    activationB = Activation_Softmax()
+
+    loss_function = Loss_CategoricalCrossentropy()
+
+
+    lowest_loss = 9999999
+    best_denseA_weights = denseA.weights.copy()
+    best_denseA_biases = denseA.biases.copy()
+
+    best_denseB_weights = denseB.weights.copy()
+    best_denseB_biases = denseB.biases.copy()
+
+
+    for iteration in range(100000):
+        denseA.weights = 0.05 * np.random.randn(2, 3)
+        denseA.biases = 0.05 * np.random.randn(1, 3)
+
+        denseB.weights = 0.05 * np.random.randn(3,3)
+        denseB.biases = 0.05 * np.random.randn(1, 3)
+
+        denseA.forward(A)
+        activationA.forward(denseA.output)
+
+        denseB.forward(activationA.output)
+        activationB.forward(denseB.output)
+
+
+        loss = loss_function.calculate(activationB.output, b)
+
+        predictions = np.argmax(activationB.output, axis=1)
+        accuracy = np.mean(predictions==y)
+
+
+        if loss < lowest_loss:
+            print("New set of weights found, iteration: ", iteration, "loss: ", loss, "acc: ", accuracy)
+            best_denseA_weights = denseA.weights.copy()
+            best_denseA_biases = denseA.biases.copy()
+            best_denseB_weights = denseB.weights.copy()
+            best_denseB_biases = denseB.biases.copy()
+
+            lowest_loss = loss
+
+
